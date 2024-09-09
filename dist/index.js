1eb406e902340daabd974e1f4aa24c869cf0815d
Claire Devillers <clairedevillersca@gmail.com>
2024-09-09 13:06:22 +0200

    get and post are done

--- dist/index.js
+++ dist/index.js
@@ -4,19 +4,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// ! We create an instance of an express app/server by calling the express function.
+// ! We create an instance of an express app/server by calling this express function
 const app = (0, express_1.default)();
-// ! It needs to be able to handle different ROUTES.
-// ? setup router
+const movieData = [
+    { name: "Long Legs", id: 1 },
+    { id: 2, name: "Notting Hill" },
+    { id: 3, name: "Zombieland" }
+];
+// ! It helps us handle different ROUTES (pokemon.com/api/pikachu)
 const router = express_1.default.Router();
-// ? use the router method, with a get method
-router.route('/api/hello').get((req, res) => {
-    res.send('Hello back');
+// ! Getting all the movies. sample url /movies
+router.route('/api/movies').get((req, res) => {
+    res.send(movieData);
 });
-// ? use the router in our express app
+// ! getting an individual movie. sample url /movies/7
+// ? :id lets us invent a wildcard(url param) so we can accept urls of the form
+//        /api/movies/1 or /api/movies/2 or ...
+//        (then req.params.id variable will contain the "2", e.g.)
+router.route('/api/movies/:id').get((req, res) => {
+    const incomingMovieID = Number(req.params.id);
+    console.log("incoming movie ID is", incomingMovieID);
+    let foundMovie = movieData.find((movieObj) => {
+        return movieObj.id === incomingMovieID;
+    });
+    res.send(foundMovie);
+});
+// POST a movie to the server. sample url: POST /movies
+// ? req.body is a variable that contains data submitted from the client
+//   to our api server
+router.route('/api/movies').post((req, res) => {
+    console.log("user sent us a JSON", req.body);
+    const submittedMovie = req.body;
+    submittedMovie.id = movieData.length + 1; // a hack to always increment ids
+    movieData.push(submittedMovie);
+    //res.send(movieData)
+    res.status(200).json(movieData);
+});
+// TODO: UPDATE a movie on srv. sample url: PUT /movies/7
+// TODO: DELETE a movie on srv. sample url DELETE /movies/2
+// ? use the router's .route() method, with another method called .get()
+// ? req - a variable that contains useful information about the user's incoming REQUEST
+// ? res - a variable that contains method to help us RESPOND to the user
+router.route('/api/bulbasaur').get((req, res) => {
+    res.send("Bulbasaur says Hello: ^ö^");
+});
+// just a demo of a catchall route to handle invalid requests
+router.route('/*').get((req, res) => {
+    res.send("404");
+});
+// ! To get POSTing to work, we need to add this line:
+app.use(express_1.default.json());
+// ? use the router we defined above, in our express app we created above
 app.use(router);
-// ! Program needs to be running all the time -- otherwise you get downtime. It has to be available.
-// ? first argument is a PORT number, second is a function
-app.listen(8000, () => {
-    console.log('Express API is running on http://localhost:8000');
+// ! Our webserver needs to be running(listening) at all times -- otherwise you get downtime.
+// ? first argument is a PORT number (typically 80 on production)
+app.listen(8001, () => {
+    console.log("Hey i'm running express! Yay. On localhost:8000");
 });
--- index.ts
+++ index.ts
@@ -1,1 +1,70 @@
-// TODO: make API server here
+import express from 'express'
+
+// ! We create an instance of an express app/server by calling this express function
+const app = express()
+
+const movieData = [
+    { name: "Long Legs", id: 1},
+    { id: 2, name: "Notting Hill"},
+    { id: 3, name: "Zombieland"}
+]
+
+// ! It helps us handle different ROUTES (pokemon.com/api/pikachu)
+const router = express.Router()
+
+// ! Getting all the movies. sample url /movies
+router.route('/api/movies').get((req,res) => {
+    res.send(movieData)
+})
+
+// ! getting an individual movie. sample url /movies/7
+// ? :id lets us invent a wildcard(url param) so we can accept urls of the form
+//        /api/movies/1 or /api/movies/2 or ...
+//        (then req.params.id variable will contain the "2", e.g.)
+router.route('/api/movies/:id').get((req,res) => {
+    const incomingMovieID = Number(req.params.id)
+    console.log("incoming movie ID is", incomingMovieID)
+    let foundMovie = movieData.find((movieObj)=>{
+        return movieObj.id === incomingMovieID
+    })
+    res.send(foundMovie)
+})
+
+// POST a movie to the server. sample url: POST /movies
+// ? req.body is a variable that contains data submitted from the client
+//   to our api server
+router.route('/api/movies').post((req,res)=>{
+    console.log("user sent us a JSON", req.body)
+    const submittedMovie = req.body
+    submittedMovie.id = movieData.length + 1 // a hack to always increment ids
+    movieData.push(submittedMovie)
+    //res.send(movieData)
+    res.status(200).json(movieData)
+})
+
+// TODO: UPDATE a movie on srv. sample url: PUT /movies/7
+// TODO: DELETE a movie on srv. sample url DELETE /movies/2
+
+// ? use the router's .route() method, with another method called .get()
+// ? req - a variable that contains useful information about the user's incoming REQUEST
+// ? res - a variable that contains method to help us RESPOND to the user
+router.route('/api/bulbasaur').get((req,res) => {
+    res.send("Bulbasaur says Hello: ^ö^")
+})
+
+// just a demo of a catchall route to handle invalid requests
+router.route('/*').get((req,res) => {
+    res.send("404")
+})
+
+// ! To get POSTing to work, we need to add this line:
+app.use(express.json())
+
+// ? use the router we defined above, in our express app we created above
+app.use(router)
+
+// ! Our webserver needs to be running(listening) at all times -- otherwise you get downtime.
+// ? first argument is a PORT number (typically 80 on production)
+app.listen(8001, () => {
+    console.log("Hey i'm running express! Yay. On localhost:8000")
+})
